af66bcb3a2fb6bff2c245965b59add805e29c8a0
Phuc. Tran Nguyen Hoang <phuctnh@vng.com.vn>
2022-01-22 20:42:28 +0700

    Init project

---
+++ src/main/java/org/store/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package org.store.controller;
+
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "api")
+public class ArticleController {
+
+}
